13d49725872e3bb7a65c641030685d4eefdb4b42
mortech46 <admin@mortechsystems.com>
2023-03-24 17:20:11 +0000

    Cognito logging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -77,8 +77,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -147,10 +147,11 @@ def data_create_message():
     return model['data'], 200
   return
 
-#@app.route("/api/activities/home", methods=['GET'])
-#def data_home():
-  #data = HomeActivities.run(logger=LOGGER)
-  #return data, 200
+@app.route("/api/activities/home", methods=['GET'])
+def data_home():
+  
+  data = HomeActivities.run()
+  return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_notifications():
