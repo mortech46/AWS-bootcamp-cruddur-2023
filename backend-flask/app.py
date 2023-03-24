5802b2cd125840dab4548f77c7cfc76df60fbc7c
mortech46 <admin@mortechsystems.com>
2023-03-24 20:01:34 +0000

    JWT Token FIX

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -61,7 +61,7 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
-cognito_token_verification = CognitoTokenVerification(
+cognito_jwt_token = CognitoJwtToken(
   user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
   user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"), 
   region=os.getenv("AWS_DEFAULT_REGION")
